8a3744b8ec1c2545f1b67f5f009104a2418e8668
Felix Wisser <fwisser@gmx.de>
2016-05-02 21:08:05 +0200

    + extended options menu on/off + dummy options.xml + loading data from options to spinners + labels on input elements in new game screen

--- app/src/main/java/com/mc1/dev/goapp/NewGame.java
+++ app/src/main/java/com/mc1/dev/goapp/NewGame.java
@@ -1,22 +1,31 @@
 package com.mc1.dev.goapp;
 
-import android.support.annotation.NonNull;
+
+import android.content.res.XmlResourceParser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.test.suitebuilder.annotation.Suppress;
 import android.view.View;
+import android.net.Uri;
+import android.view.ViewStub;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import org.jetbrains.annotations.NotNull;
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+
 
 public class NewGame extends AppCompatActivity {
 
-
-    private TextView switchStatus;
-    private Switch  extendedOptSwitch;
+    private Switch extendedOptSwitch;
     private Switch randomBnWSwitch;
+    private ViewStub extendedOptionsStub;
+    private View extendedOptionsView;
+    private Spinner mapSizeSpinner;
 
     // ----------------------------------------------------------------------
     // function onCreate
@@ -26,6 +35,13 @@ public class NewGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
+        extendedOptionsStub = (ViewStub) findViewById(R.id.extendedOptionsStub);
+
+        mapSizeSpinner = (Spinner) findViewById(R.id.mapsizeSpinner);
+        if (mapSizeSpinner != null) {
+            fillSpinner(mapSizeSpinner, fetchMapSizeElements());
+        }
+
         // --------------------------------------------
         // handle extendedOptionsSwitch
         // --------------------------------------------
@@ -33,14 +49,28 @@ public class NewGame extends AppCompatActivity {
         extendedOptSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
-            public void onCheckedChanged(CompoundButton switchView, boolean isChecked) {
+            public void onCheckedChanged(CompoundButton switchView, boolean isChecked){
+                if (extendedOptSwitch != null) {
+                    if(isChecked){
+                        extendedOptSwitch.setText(R.string.extended_options_switch_on);
+                        // handle extended options menu
+                        if (extendedOptionsView == null) { // initial set to "on"
+                            extendedOptionsStub.inflate();
+                            fillExtendedOptions();  // load data from options xml
+                            extendedOptionsView = findViewById(R.id.extendedOptionsView);
+                        }
+                        else { // every other case of switch being flipped
+                            extendedOptionsView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                    else{
+                        extendedOptSwitch.setText(R.string.extended_options_switch_off);
+                        if (extendedOptionsView != null) { // protect from initial missing view, as it is inflated on first call
+                            extendedOptionsView.setVisibility(View.GONE);
+                        }
+                    }
+                }
 
-                if(isChecked){
-                    extendedOptSwitch.setText(R.string.extended_options_switch_on);
-                }
-                else{
-                    extendedOptSwitch.setText(R.string.extended_options_switch_off);
-                }
             }
         });
 
@@ -71,4 +101,157 @@ public class NewGame extends AppCompatActivity {
             }
         });
     }
+
+    // ----------------------------------------------------------------------
+    // function onFragmentInteraction
+    // handles data interaction with the extended options fragment
+    // if the extended options switch is turned on
+    // ----------------------------------------------------------------------
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    // ----------------------------------------------------------------------
+    // function fillSpinner
+    // creates an adapter for input spinners and transforms given data
+    // to option elements
+    // ----------------------------------------------------------------------
+    public void fillSpinner(Spinner spinner, ArrayList<String> elements) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, elements);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    // ----------------------------------------------------------------------
+    // function fetchMapSizeElements()
+    // fetches data for map sizes from the options resource file
+    // @return all configured map size choices
+    // ----------------------------------------------------------------------
+    private ArrayList<String> fetchMapSizeElements() {
+        XmlResourceParser xrp = getResources().getXml(R.xml.options);
+        ArrayList<String> allSizes = new ArrayList<>();
+
+        try {
+            int eventType = xrp.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch(eventType) {
+                    case XmlPullParser.START_TAG :
+                        if (xrp.getName().equals("Size")) {
+                            String oneNumber = xrp.getAttributeValue(null, "value");
+                            allSizes.add(oneNumber + " x " + oneNumber); // create the strings for the "height x width" options
+                        }
+                        break;
+                }
+                eventType = xrp.next();
+            }
+            xrp.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return allSizes;
+    }
+
+
+    // ----------------------------------------------------------------------
+    // function fillExtendedOptions
+    // fills the input-elements of the extended options
+    // menu. also picks the needed elements from the layout
+    //
+    // needs to be called after inflating the extendedOptionsStub to a full view
+    // only needs to be called once! in the whole activity lifecycle
+    //
+    // calls fillHandicapSeekbar
+    //
+    // ----------------------------------------------------------------------
+
+    private void fillExtendedOptions() {
+        Spinner timeModeSpinner = (Spinner) findViewById(R.id.timeModeSpinner);
+        Spinner komiSpinner = (Spinner) findViewById(R.id.komiSpinner);
+
+        if (timeModeSpinner != null && komiSpinner != null) {
+            fillSpinner(timeModeSpinner, fetchTimeModeElements());
+            fillSpinner(komiSpinner, fetchKomiElements());
+        }
+    }
+
+    // ----------------------------------------------------------------------
+    // function fetchTimeModeElements()
+    // fetches data for map sizes from the options resource file
+    // @return all configured time mode choices
+    // ----------------------------------------------------------------------
+    private ArrayList<String> fetchTimeModeElements() {
+        XmlResourceParser xrp = getResources().getXml(R.xml.options);
+        ArrayList<String> allModes = new ArrayList<>();
+
+        try {
+            int eventType = xrp.getEventType();
+            String text = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch(eventType) {
+                    case XmlPullParser.TEXT : text = xrp.getText(); break;
+                    case XmlPullParser.END_TAG :
+                        if (xrp.getName().equals("Mode")) {
+                            allModes.add(text);
+                        }
+                        break;
+                }
+                eventType = xrp.next();
+            }
+            xrp.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return allModes;
+    }
+
+    // ----------------------------------------------------------------------
+    // function fetchKomiElements()
+    // fetches data for map sizes from the options resource file
+    // @return all configured komi choices
+    // ----------------------------------------------------------------------
+    private ArrayList<String> fetchKomiElements() {
+        XmlResourceParser xrp = getResources().getXml(R.xml.options);
+        ArrayList<String> allKomi = new ArrayList<>();
+
+        try {
+            int eventType = xrp.getEventType();
+            String text = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch(eventType) {
+                    case XmlPullParser.TEXT: text = xrp.getText(); break;
+                    case XmlPullParser.END_TAG :
+                        if (xrp.getName().equals("Komi")) {
+                            allKomi.add(text);
+                        }
+                        break;
+                }
+                eventType = xrp.next();
+            }
+            xrp.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return allKomi;
+    }
+
+    // ----------------------------------------------------------------------
+    // function fillHandicapSeekbar
+    // reads the data from options resource file and fills the spinner with
+    // the parsed data
+    //
+    // needs to be called after inflating the extendedOptionsStub to a full
+    // view, if not, the spinner will be empty
+    // called in fillExtendedOptions
+    // ----------------------------------------------------------------------
+    private void fillHandicapSeekbar(SeekBar handicapSeekbar) {
+        // todo
+    }
+
 }
