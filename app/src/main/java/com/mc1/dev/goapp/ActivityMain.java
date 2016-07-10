23b90ab78e20a40e41543b710a97f8d5960950a1
Hendrik Schmidt <hfrschmidt@gmail.com>
2016-07-10 21:18:59 +0200

    Added tutorial text including clickable links for a more detailed tutorial.

--- app/src/main/java/com/mc1/dev/goapp/ActivityMain.java
+++ app/src/main/java/com/mc1/dev/goapp/ActivityMain.java
@@ -3,18 +3,18 @@ package com.mc1.dev.goapp;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.SubMenu;
-import android.view.View;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -84,7 +84,13 @@ public class ActivityMain extends AppCompatActivity {
     }
 
     public void tutorialDialog(View view) {
-        dialogBuilder.setMessage(R.string.dialog_tutorial_content).setTitle(R.string.dialog_tutorial_title);
+        final SpannableString s = new SpannableString(getText(R.string.dialog_tutorial_content));
+        Linkify.addLinks(s, Linkify.WEB_URLS);
+        final TextView message = new TextView(this);
+        message.setText(s);
+        message.setMovementMethod(LinkMovementMethod.getInstance());
+        message.setPadding(30, 5, 30, 5);
+        dialogBuilder.setTitle(R.string.dialog_tutorial_title).setView(message);
         dialogBuilder.show();
     }
 
