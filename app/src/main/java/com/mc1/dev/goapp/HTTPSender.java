4c8e867b3dd6b3f6ebf8066cb99a58298962a3b0
Felix Wisser <fwisser@gmx.de>
2016-12-08 12:43:02 +0100

    implemented rest methods

--- app/src/main/java/com/mc1/dev/goapp/HTTPSender.java
+++ app/src/main/java/com/mc1/dev/goapp/HTTPSender.java
@@ -1,5 +1,11 @@
 package com.mc1.dev.goapp;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.logging.Level;
 
 /**
@@ -7,7 +13,7 @@ import java.util.logging.Level;
  */
 public class HTTPSender {
 
-    private final String SERVERURL = "localhost/goserver"; // change to hosted url
+    private final String SERVERURL = "localhost:8080/goserver"; // change to hosted url
 
     public HTTPSender() {
 
@@ -16,27 +22,24 @@ public class HTTPSender {
     public void postMatch(String token, String JSONData) {
         HttpURLConnection connection = null;
 
-        jsobj.put("to", token);
-
         try {
             //Create connection
             URL url = new URL(SERVERURL + "/match/" + token);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Authorization", "key=" + SERVERKEY);
 
             connection.setUseCaches(false);
             connection.setDoOutput(true);
 
             //Send request
             DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
-            wr.writeBytes(jsobj.toJSONString());
+            wr.writeBytes(""); // -> this might not work, but on get request only the headers are needed, so what else to write?
             wr.close();
 
         } catch (Exception e) {
-            LOGGER.log(Level.ALL, e.getMessage());
-            return false;
+            e.getStackTrace();
+            e.getMessage();
         }
 
         // catch response
@@ -50,17 +53,15 @@ public class HTTPSender {
                 response.append('\r');
             }
             rd.close();
-            return checkResponse(response.toString());
+            // response.toString() -> full response message
         } catch (Exception e) {
-            LOGGER.log(Level.ALL, e.getMessage());
-            return false;
+            e.getStackTrace();
+            e.getMessage();
         }
     }
 
     public void deleteMatch(String token) {
         HttpURLConnection connection = null;
-
-        jsobj.put("to", token);
 
         try {
             //Create connection
@@ -74,7 +75,7 @@ public class HTTPSender {
 
             //Send request
             DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
-            wr.writeBytes(jsobj.toJSONString());
+            wr.writeBytes("");
             wr.close();
 
         } catch (Exception e) {
@@ -93,7 +94,90 @@ public class HTTPSender {
                 response.append('\r');
             }
             rd.close();
-            return checkResponse(response.toString());
+            // response.toString() -> full response
+        } catch (Exception e) {
+            e.getStackTrace();
+            e.getMessage();
+        }
+    }
+
+    public void postMove(String token, String JSONData) {
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            URL url = new URL(SERVERURL + "/play/" + token);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
+            wr.writeBytes(JSONData);
+            wr.close();
+
+        } catch (Exception e) {
+            e.getStackTrace();
+            e.getMessage();
+        }
+
+        // catch response
+        try {
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            // response.toString() -> full response message
+        } catch (Exception e) {
+            e.getStackTrace();
+            e.getMessage();
+        }
+    }
+
+    public void getMove(String token, String JSONData) {
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            URL url = new URL(SERVERURL + "/match/" + token);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
+            wr.writeBytes(""); // -> this might not work, but on get request only the headers are needed, so what else to write?
+            wr.close();
+
+        } catch (Exception e) {
+            e.getStackTrace();
+            e.getMessage();
+        }
+
+        // catch response
+        try {
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            // response.toString() -> full response message
+            // TODO response contains move information -> do sth with it
         } catch (Exception e) {
             e.getStackTrace();
             e.getMessage();
